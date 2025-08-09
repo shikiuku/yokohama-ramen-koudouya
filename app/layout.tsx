a23f81b23aa56690e43c9c4fd7d973bdafd709a7
kuni567 <kuni567489@gmail.com>
2025-08-09 22:18:59 +0900

    feat: Google FontsのYuji Maiフォントを全体に適用
    - layout.tsxでGoogle Fontsからインポート
    - globals.cssでbody要素にfont-familyを設定
    - 毛筆風の日本語フォントでサイト全体を表示

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,4 @@
 import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -17,13 +15,9 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <head>
-        <style>{`
-html {
-  font-family: ${GeistSans.style.fontFamily};
-  --font-sans: ${GeistSans.variable};
-  --font-mono: ${GeistMono.variable};
-}
-        `}</style>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Yuji+Mai&display=swap" rel="stylesheet" />
       </head>
       <body>{children}</body>
     </html>
